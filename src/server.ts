1d2951a5051ed426087aef906474f0526e3ae0fa
lemoscaio <caio_lemos94@hotmail.com>
2022-08-10 02:50:40 -0300

    simulate commit to check PR workflow

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is up and running on port 5000!`);
+  console.log(`Server is up and running on port 5000`);
 })
